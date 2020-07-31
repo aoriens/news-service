9417c0acbef1e8158af6f242ee5f68d6a13e2a5b
Anton Kalinin <aoriens@gmail.com>
2020-07-31 14:35:28 +0300

    Make a stub application

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,22 @@
-module Main where
+{-# LANGUAGE OverloadedStrings #-}
 
-import Lib
+module Main
+  ( main
+  ) where
+
+import qualified Data.ByteString.Lazy.Char8 as BS
+import qualified Network.HTTP.Types.Header as Http
+import qualified Network.HTTP.Types.Status as Http
+import qualified Network.Wai as Wai
+import qualified Network.Wai.Handler.Warp as Warp
 
 main :: IO ()
-main = someFunc
+main = do
+  putStrLn "Server started"
+  Warp.run 4000 application
+
+application :: Wai.Application
+application request respond =
+  respond $
+  Wai.responseLBS Http.ok200 [(Http.hContentType, "text/plain")] $
+  "It works!\n" <> BS.pack (show request)
