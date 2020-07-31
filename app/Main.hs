66c0c18c93d3c50486987b97c3a663c447fbb7e9
Anton Kalinin <aoriens@gmail.com>
2020-07-31 21:52:51 +0300

    Output stub news disregarding URL

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Main
   ( main
   ) where
 
-import qualified Data.ByteString.Lazy.Char8 as BS
+import qualified Data.Aeson as A
+import Data.List
+import Data.Maybe
+import Data.Text (Text)
+import Data.Time.Calendar
+import Data.Time.Clock
+import GHC.Generics
 import qualified Network.HTTP.Types.Header as Http
 import qualified Network.HTTP.Types.Status as Http
 import qualified Network.Wai as Wai
@@ -16,7 +23,51 @@ main = do
   Warp.run 4000 application
 
 application :: Wai.Application
-application request respond =
+application _request respond =
   respond $
-  Wai.responseLBS Http.ok200 [(Http.hContentType, "text/plain")] $
-  "It works!\n" <> BS.pack (show request)
+  Wai.responseLBS Http.ok200 [(Http.hContentType, "application/json")] $
+  A.encode stubNews
+
+data News =
+  News
+    { newsTitle :: Text
+    , newsDate :: UTCTime
+    , newsText :: Text
+    , newsImage :: String
+    }
+  deriving (Generic)
+
+instance A.ToJSON News where
+  toJSON = A.genericToJSON (toJSONOptionsWithFieldPrefix "news")
+  toEncoding = A.genericToEncoding (toJSONOptionsWithFieldPrefix "news")
+
+toJSONOptionsWithFieldPrefix :: String -> A.Options
+toJSONOptionsWithFieldPrefix prefix =
+  A.defaultOptions {A.fieldLabelModifier = formatField}
+  where
+    formatField fieldName =
+      A.camelTo2 '_' .
+      fromMaybe
+        (error
+           ("The field '" ++
+            fieldName ++ "' must be prefixed with '" ++ prefix ++ "'")) .
+      stripPrefix prefix $
+      fieldName
+
+stubNews :: [News]
+stubNews =
+  [ News
+      { newsTitle = "Title1"
+      , newsDate = utcMidnight 2020 07 27
+      , newsText = "A news text"
+      , newsImage = "http://example.com/image1.png"
+      }
+  , News
+      { newsTitle = "Title2"
+      , newsDate = utcMidnight 2020 01 01
+      , newsText = "A news text"
+      , newsImage = "http://example.com/image1.png"
+      }
+  ]
+  where
+    utcMidnight year month day = UTCTime (fromGregorian year month day) 0
