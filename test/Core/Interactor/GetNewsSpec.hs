96fbe30556852105b416cfef1522c1fc20ad6fa8
Anton Kalinin <aoriens@gmail.com>
2020-08-14 21:55:58 +0300

    Add pagination tests for Core.Interactor.GetNews

--- test/Core/Interactor/GetNewsSpec.hs
+++ test/Core/Interactor/GetNewsSpec.hs
@@ -38,6 +38,33 @@ spec =
           hGetNews = \Page {..} -> writeIORef passedLimit pageLimit >> pure []
       _ <- I.getNews I.Handle {..} noPageQuery
       readIORef passedLimit `shouldReturn` hMaxPageLimit
+    it "should pass the page offset to the gateway" $ do
+      passedOffset <- newIORef (PageOffset 0)
+      let hMaxPageLimit = PageLimit 1
+          offset = PageOffset 3
+          hGetNews = \Page {..} -> writeIORef passedOffset pageOffset >> pure []
+      _ <- I.getNews I.Handle {..} noPageQuery {pageQueryOffset = Just offset}
+      readIORef passedOffset `shouldReturn` offset
+    it "should pass offset=0 to the gateway if no offset specified" $ do
+      passedOffset <- newIORef (PageOffset 0)
+      let hMaxPageLimit = PageLimit 1
+          hGetNews = \Page {..} -> writeIORef passedOffset pageOffset >> pure []
+      _ <- I.getNews I.Handle {..} noPageQuery
+      readIORef passedOffset `shouldReturn` PageOffset 0
+    it
+      "should pass the specified page limit to the gateway if less than max limit" $ do
+      passedLimit <- newIORef (PageLimit 0)
+      let hMaxPageLimit = PageLimit 10
+          limit = PageLimit 9
+          hGetNews = \Page {..} -> writeIORef passedLimit pageLimit >> pure []
+      _ <- I.getNews I.Handle {..} noPageQuery {pageQueryLimit = Just limit}
+      readIORef passedLimit `shouldReturn` limit
+    it "should pass the default limit if no one specified" $ do
+      passedLimit <- newIORef (PageLimit 0)
+      let hMaxPageLimit = PageLimit 10
+          hGetNews = \Page {..} -> writeIORef passedLimit pageLimit >> pure []
+      _ <- I.getNews I.Handle {..} noPageQuery {pageQueryLimit = Nothing}
+      readIORef passedLimit `shouldReturn` hMaxPageLimit
 
 noPageQuery :: PageQuery
 noPageQuery = PageQuery Nothing Nothing
