782e8d0626cb0151922f85bd511566ee49e2980b
Anton Kalinin <aoriens@gmail.com>
2020-08-26 10:55:25 +0300

    Add more tests for Router

--- test/Web/RouterSpec.hs
+++ test/Web/RouterSpec.hs
@@ -2,12 +2,14 @@ module Web.RouterSpec
   ( spec
   ) where
 
+import Core.Image
 import Data.IORef
 import Data.List
 import qualified Network.HTTP.Types as Http
 import qualified Network.Wai as Wai
 import qualified Network.Wai.Internal as Wai
 import Test.Hspec
+import qualified Web.AppURL as U
 import qualified Web.Router as R
 import Web.Types as Web
 import Web.Types.Internal.SessionId as Web
@@ -18,17 +20,36 @@ spec =
     it "should return a handler by URL path and HTTP method" $ do
       let path = ["my_path"]
           method = Http.methodPost
-          expectedHeader = ("X-My-Header", "")
-          expectedHandler _ _ =
-            ($ Wai.responseLBS Http.ok200 [expectedHeader] "")
+          expectedHandler = stubHandlerWithHeader ("X-My-Header", "")
           router = R.new $ R.ifPath path $ R.ifMethod method expectedHandler
           request =
             Wai.defaultRequest {Wai.pathInfo = path, Wai.requestMethod = method}
           (R.HandlerResult handler) = R.route router request
-      (_, headers) <- getResponse (handler stubSession) request
-      headers `shouldContain` [expectedHeader]
+      handler `shouldEmitSameHeadersAs` expectedHandler
+    it
+      "should return a handler matching an AppURL if an AppURL-decodable path is passed" $ do
+      let appURL = U.URLImage (ImageId 1)
+          method = Http.methodGet
+          expectedHandler = stubHandlerWithHeader ("X-My-Header", "")
+          router =
+            R.new $
+            R.ifAppURL $ \U.URLImage {} -> R.ifMethod method expectedHandler
+          request =
+            Wai.defaultRequest
+              { Wai.pathInfo = U.relativeURLPath $ U.toRelativeURL appURL
+              , Wai.requestMethod = method
+              }
+          (R.HandlerResult handler) = R.route router request
+      handler `shouldEmitSameHeadersAs` expectedHandler
     it "should return ResourceNotFoundRequest for unknown URL path" $ do
       let router = R.new $ pure ()
+          request = Wai.defaultRequest {Wai.pathInfo = ["unknown_path"]}
+          result = R.route router request
+      result `shouldSatisfy` R.isResourceNotFoundResult
+    it
+      "should return ResourceNotFoundRequest for unknown URL path even when ifAppURL is used" $ do
+      let router =
+            R.new $ R.ifAppURL $ \U.URLImage {} -> R.ifMethod "GET" noOpHandler
           request = Wai.defaultRequest {Wai.pathInfo = ["unknown_path"]}
           result = R.route router request
       result `shouldSatisfy` R.isResourceNotFoundResult
@@ -49,14 +70,23 @@ spec =
       methods `shouldBe`
         sort [Http.methodPost, Http.methodPut, Http.methodDelete]
 
-getResponse ::
-     Wai.Application -> Wai.Request -> IO (Http.Status, Http.ResponseHeaders)
-getResponse app request = do
+stubHandlerWithHeader :: Http.Header -> EApplication
+stubHandlerWithHeader header _ _ = ($ Wai.responseLBS Http.ok200 [header] "")
+
+shouldEmitSameHeadersAs ::
+     HasCallStack => EApplication -> EApplication -> Expectation
+shouldEmitSameHeadersAs a1 a2 = do
+  headers1 <- getHeaders (a1 stubSession) Wai.defaultRequest
+  headers2 <- getHeaders (a2 stubSession) Wai.defaultRequest
+  headers1 `shouldBe` headers2
+
+getHeaders :: Wai.Application -> Wai.Request -> IO Http.ResponseHeaders
+getHeaders app request = do
   result <- newIORef (error "The response continuation must be invoked")
   _ <-
     app request $ \response -> do
-      let (status, headers, _) = Wai.responseToStream response
-      writeIORef result (status, headers)
+      let (_, headers, _) = Wai.responseToStream response
+      writeIORef result headers
       pure Wai.ResponseReceived
   readIORef result
 
