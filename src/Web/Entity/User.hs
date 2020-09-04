eef1978d2c3f1d32bd5dedf3cd354560150ee954
Anton Kalinin <aoriens@gmail.com>
2020-09-04 19:28:42 +0300

    Move out Web.User.Entity from UserPresenter

---
+++ src/Web/Entity/User.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Web.Entity.User
+  ( User
+  , userEntity
+  ) where
+
+import Core.Authentication as Core
+import qualified Core.User as Core
+import qualified Data.Aeson as A
+import qualified Data.Aeson.TH as A
+import Data.Int
+import Data.List
+import Data.Maybe
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+import Data.Time
+import qualified Web.AppURL as U
+import Web.Credentials
+
+data User =
+  User
+    { userId :: Int32
+    , userFirstName :: Maybe T.Text
+    , userLastName :: T.Text
+    , userAvatarURL :: Maybe T.Text
+    , userCreatedAt :: UTCTime
+    , userIsAdmin :: Bool
+    , userSecretToken :: Maybe T.Text
+    }
+
+userEntity ::
+     (U.AppURL -> T.Text) -> Maybe Core.Credentials -> Core.User -> User
+userEntity renderAppURL creds Core.User {..} =
+  User
+    { userId = Core.getUserId userId
+    , userFirstName = userFirstName
+    , userLastName = userLastName
+    , userAvatarURL = renderAppURL . U.URLImage <$> userAvatarId
+    , userCreatedAt = userCreatedAt
+    , userIsAdmin = userIsAdmin
+    , userSecretToken =
+        T.decodeLatin1 . unWebToken . presentCredentials <$> creds
+    }
+
+$(A.deriveToJSON
+    A.defaultOptions
+      { A.fieldLabelModifier = A.camelTo2 '_' . fromJust . stripPrefix "user"
+      , A.omitNothingFields = True
+      }
+    ''User)
--- src/Web/Presenter/UserPresenter.hs
+++ src/Web/Presenter/UserPresenter.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE RankNTypes #-}
 
 module Web.Presenter.UserPresenter
@@ -10,16 +9,10 @@ module Web.Presenter.UserPresenter
 import qualified Core.Authentication as Auth
 import qualified Core.User as C
 import qualified Data.Aeson as A
-import qualified Data.Aeson.TH as A
 import qualified Data.ByteString.Builder as BB
-import Data.Int
-import Data.List
-import Data.Maybe
 import Data.Text (Text)
-import qualified Data.Text.Encoding as T
-import Data.Time
 import qualified Web.AppURL as U
-import Web.Credentials
+import Web.Entity.User
 
 data Handle =
   Handle
@@ -29,38 +22,8 @@ data Handle =
     }
 
 presentUser :: Handle -> C.User -> Maybe Auth.Credentials -> BB.Builder
-presentUser h user creds = hJSONEncode h $ userEntity h creds user
+presentUser h user creds =
+  hJSONEncode h $ userEntity (hRenderAppURL h) creds user
 
 presentUsers :: Handle -> [C.User] -> BB.Builder
-presentUsers h = hJSONEncode h . map (userEntity h Nothing)
-
-userEntity :: Handle -> Maybe Auth.Credentials -> C.User -> User
-userEntity h creds C.User {..} =
-  User
-    { userId = C.getUserId userId
-    , userFirstName = userFirstName
-    , userLastName = userLastName
-    , userAvatarURL = hRenderAppURL h . U.URLImage <$> userAvatarId
-    , userCreatedAt = userCreatedAt
-    , userIsAdmin = userIsAdmin
-    , userSecretToken =
-        T.decodeLatin1 . unWebToken . presentCredentials <$> creds
-    }
-
-data User =
-  User
-    { userId :: Int32
-    , userFirstName :: Maybe Text
-    , userLastName :: Text
-    , userAvatarURL :: Maybe Text
-    , userCreatedAt :: UTCTime
-    , userIsAdmin :: Bool
-    , userSecretToken :: Maybe Text
-    }
-
-$(A.deriveToJSON
-    A.defaultOptions
-      { A.fieldLabelModifier = A.camelTo2 '_' . fromJust . stripPrefix "user"
-      , A.omitNothingFields = True
-      }
-    ''User)
+presentUsers h = hJSONEncode h . map (userEntity (hRenderAppURL h) Nothing)
