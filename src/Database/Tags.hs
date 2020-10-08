07435e85c1197ea095db3fa739dbc1356e00a5b8
Anton Kalinin <aoriens@gmail.com>
2020-10-08 10:21:08 +0300

    Deduplicate tags columns in Database.Tags

--- src/Database/Tags.hs
+++ src/Database/Tags.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE ApplicativeDo #-}
 
 module Database.Tags
   ( findTagByName
@@ -7,21 +8,20 @@ module Database.Tags
   ) where
 
 import Core.Tag
+import Data.Functor.Contravariant
 import Data.Profunctor
 import qualified Data.Text as T
 import Database
+import Database.Columns
+import qualified Hasql.Decoders as D
+import qualified Hasql.Encoders as E
 import qualified Hasql.TH as H
 
 findTagByName :: Statement T.Text (Maybe Tag)
-findTagByName =
-  rmap
-    (fmap $ \(tid, tagName) -> Tag {tagId = TagId tid, tagName})
-    [H.maybeStatement|
-      select tag_id :: integer, name :: varchar
-      from tags
-      where name = $1 :: varchar
-      limit 1
-    |]
+findTagByName = statementWithColumns sql encoder tagColumns D.rowMaybe True
+  where
+    sql = "select $COLUMNS from tags where name = $1"
+    encoder = E.param (E.nonNullable E.text)
 
 createTagNamed :: T.Text -> Transaction Tag
 createTagNamed tagName = do
@@ -39,12 +39,16 @@ createTagNamedSt =
     |]
 
 findTagById :: Statement TagId (Maybe Tag)
-findTagById =
-  dimap
-    getTagId
-    (fmap $ \(tid, tagName) -> Tag {tagId = TagId tid, tagName})
-    [H.maybeStatement|
-      select tag_id :: integer, name :: varchar
-      from tags
-      where tag_id = $1 :: integer
-    |]
+findTagById = statementWithColumns sql encoder tagColumns D.rowMaybe True
+  where
+    sql = "select $COLUMNS from tags where tag_id = $1"
+    encoder = getTagId >$< E.param (E.nonNullable E.int4)
+
+tagColumns :: Columns Tag
+tagColumns = do
+  tagId <- TagId <$> column tagsTable "tag_id"
+  tagName <- column tagsTable "name"
+  pure Tag {..}
+
+tagsTable :: TableName
+tagsTable = "tags"
