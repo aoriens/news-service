02c475850c6e4ba76e991f3cf822ee70d4de4620
Anton Kalinin <aoriens@gmail.com>
2020-10-28 09:41:43 +0300

    Database.News: return list, not a vector

--- src/Database/News.hs
+++ src/Database/News.hs
@@ -6,17 +6,19 @@ module Database.News
 
 import Core.News
 import Core.Pagination
+import Data.Foldable
 import Data.Profunctor
-import Data.Vector (Vector)
 import Database
 import qualified Hasql.TH as TH
 
-getNews :: Statement PageSpec (Vector News)
+getNews :: Statement PageSpec [News]
 getNews =
   dimap
     (\PageSpec {..} -> (getPageLimit pageLimit, getPageOffset pageOffset))
-    (fmap $ \(rawId, newsTitle, newsDate, newsText) ->
-       News {newsId = NewsId rawId, ..})
+    (map
+       (\(rawId, newsTitle, newsDate, newsText) ->
+          News {newsId = NewsId rawId, ..}) .
+     toList)
     [TH.vectorStatement|
     select news_id :: integer, title :: varchar, date :: date, body :: varchar
     from news join news_versions using (news_version_id)
--- src/Gateway/News.hs
+++ src/Gateway/News.hs
@@ -4,10 +4,8 @@ module Gateway.News
 
 import Core.News
 import Core.Pagination
-import Data.Foldable
 import qualified Database as DB
 import qualified Database.News as DNews
 
 getNews :: DB.Handle -> PageSpec -> IO [News]
-getNews h page =
-  toList <$> DB.runTransactionRO h (DB.statement DNews.getNews page)
+getNews h page = DB.runTransactionRO h (DB.statement DNews.getNews page)
