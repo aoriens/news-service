b50ea73e25033dc1a653b627afae9c89d907e214
Anton Kalinin <aoriens@gmail.com>
2020-12-16 22:07:29 +0300

    Fix warning: out-of-bound integer literal

--- test/Data/Integral/ExactSpec.hs
+++ test/Data/Integral/ExactSpec.hs
@@ -20,7 +20,8 @@ spec =
         Just (fromIntegral x)
     it "should convert to a narrower type if preserves exact value" $ do
       (fromIntegralExact (127 :: Int) :: Maybe Int8) `shouldBe` Just 127
-      (fromIntegralExact (-128 :: Int) :: Maybe Int8) `shouldBe` Just (-128)
+      (fromIntegralExact (-128 :: Int) :: Maybe Int8) `shouldBe`
+        Just (fromIntegral (-128 :: Int))
       (fromIntegralExact (255 :: Int) :: Maybe Word8) `shouldBe` Just 255
       (fromIntegralExact (0 :: Int) :: Maybe Word) `shouldBe` Just 0
     it "should return Nothing when exact conversion is impossible" $ do
