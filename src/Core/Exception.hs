9f9fed487560cb8aabf0233ab5b21eea2f275544
Anton Kalinin <aoriens@gmail.com>
2020-12-23 10:57:49 +0300

    Document obsolete core exceptions

--- src/Core/Exception.hs
+++ src/Core/Exception.hs
@@ -23,13 +23,18 @@ type ContentType = Text
 -- | An exception to designate incorrect input data to an interactor.
 data CoreException
   = IncorrectParameterException Reason
+  -- ^ Obsolete. It's better to use 'Either' with a use-case-specific
+  -- failure type to allow the caller to analyze and handle the error.
   | BadCredentialsException Reason
   | AuthenticationRequiredException
-   -- ^ should be used when no permission check is performed;
+   -- ^ Should be used when no permission check is performed;
    -- otherwise 'NoPermissionException' is more appropriate.
   | NoPermissionException Permission ActionName
   | RequestedEntityNotFoundException EntityId
+  -- ^ Obsolete. Use 'Maybe' or 'Either' with a use-case-specific
+  -- return type instead, so that the caller could handle it.
   | DependentEntitiesNotFoundException [EntityId]
+  -- ^ Obsolete, similar to 'IncorrectParameterException'.
   | DisallowedImageContentTypeException ContentType [ContentType]
   deriving (Show, Eq)
 
